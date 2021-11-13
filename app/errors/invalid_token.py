9824d34b1122ca966aec6014f6b05348da8630e7
Jonathan Leijendekker <jonathanleijendekker@gmail.com>
2021-11-13 22:18:59 +0800

    fixed errors

--- app/errors/invalid_token.py
+++ app/errors/invalid_token.py
@@ -1,16 +1,16 @@
 from app.data.error_response import ErrorResponse, ErrorSource
 from app.errors.exceptions.invalid_token_exception import InvalidTokenException
 from app.resources.messages.message import get_message
+from starlette.responses import JSONResponse
 
 
-async def invalid_token_handler(
-    _,
-    exc: InvalidTokenException
-) -> ErrorResponse:
+async def invalid_token_handler(_, exc: InvalidTokenException) -> JSONResponse:
     reason = exc.args[0]
     sources = ['Token']
     code = 'access.invalid'
     message = get_message(code, reason)
     source = ErrorSource(sources=sources, code=code, message=message)
+    status_code = 400
+    response = ErrorResponse([source], status_code)
 
-    return ErrorResponse([source], 400)
+    return JSONResponse(response.dict(), status_code)
