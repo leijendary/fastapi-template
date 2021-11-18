1e4e697a92dfbf09385700d869477f0b8b4273ae
Jonathan Leijendekker <jonathanleijendekker@gmail.com>
2021-11-18 17:32:18 +0800

    added sample_out openapi

--- app/api/v1/data/sample_out.py
+++ app/api/v1/data/sample_out.py
@@ -3,13 +3,27 @@ from typing import List
 from uuid import UUID
 
 from app.api.v1.data.sample_translation_out import SampleTranslationOut
+from app.core.libraries.message import get_message
 from pydantic import BaseModel
+from pydantic.fields import Field
 
 
 class SampleOut(BaseModel):
     id: UUID
-    column_1: str
-    column_2: str
-    translations: List[SampleTranslationOut] = []
-    created_at: datetime
-    modified_at: datetime
+    column_1: str = Field(
+        ...,
+        title=get_message('document.sample_column_1'),
+        max_length=100
+    )
+    column_2: str = Field(..., title=get_message('document.sample_column_2'))
+    translations: List[SampleTranslationOut] = Field(
+        [],
+        title=get_message('document.translation_list'),
+        min_items=1,
+        unique_list=True
+    )
+    created_at: datetime = Field(..., title=get_message('document.created_at'))
+    modified_at: datetime = Field(
+        ...,
+        title=get_message('document.modified_at')
+    )
--- app/api/v1/data/sample_translation_out.py
+++ app/api/v1/data/sample_translation_out.py
@@ -1,8 +1,20 @@
 from typing import Optional
 
 from app.core.data.translation_out import TranslationOut
+from app.core.libraries.message import get_message
+from pydantic.fields import Field
 
 
 class SampleTranslationOut(TranslationOut):
-    name: str
-    description: Optional[str]
+    name: str = Field(
+        ...,
+        title=get_message('document.translation_name'),
+        min_length=1,
+        max_length=150
+    )
+    description: Optional[str] = Field(
+        None,
+        title=get_message('document.translation_description'),
+        min_length=1,
+        max_length=1000
+    )
