8d32e94b846189ab2ac6236b17e682df97770584
Jonathan Leijendekker <jonathanleijendekker@gmail.com>
2021-12-01 12:50:41 +0800

    changed variable

--- app/core/security/schemes.py
+++ app/core/security/schemes.py
@@ -2,7 +2,6 @@ from app.configs.security_config import security_config
 from fastapi.security import OAuth2PasswordBearer
 
 _config = security_config()
-token_url = _config.token_url
 
 scopes = {
     scope: ''
@@ -10,7 +9,7 @@ scopes = {
 }
 
 oauth2_scheme = OAuth2PasswordBearer(
-    token_url,
+    _config.token_url,
     'Bearer',
     scopes,
     auto_error=False
