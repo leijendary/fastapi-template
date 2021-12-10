0c7b6e9cbcf08c71fb046493186f282a1d25d321
Jonathan Leijendekker <jonathanleijendekker@gmail.com>
2021-12-10 17:26:14 +0800

    added custom uvicorn worker

---
+++ app/core/workers/uvicorn_worker.py
@@ -0,0 +1,8 @@
+from uvicorn.workers import UvicornWorker
+
+
+class AppUvicornWorker(UvicornWorker):
+    CONFIG_KWARGS = {
+        **UvicornWorker.CONFIG_KWARGS,
+        'server_header': False
+    }
--- prestart.sh
+++ prestart.sh
@@ -1,2 +1,4 @@
 #! /bin/sh
 aerich upgrade
+
+export WORKER_CLASS=app.core.workers.uvicorn_worker.AppUvicornWorker
