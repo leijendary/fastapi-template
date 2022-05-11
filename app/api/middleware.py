07ecb680bcf3e547c880e8a731de5120b25995fd
Jonathan Leijendekker <jonathanleijendekker@gmail.com>
2022-05-12 00:08:18 +0800

    added prometheus

--- app/api/middleware.py
+++ app/api/middleware.py
@@ -1,6 +1,7 @@
 from app.core.plugins.request_plugin import AuthorizationPlugin, LanguagePlugin
 from fastapi import FastAPI
 from starlette_context.middleware.raw_middleware import RawContextMiddleware
+from starlette_prometheus import PrometheusMiddleware
 
 middlewares = {
     RawContextMiddleware: {
@@ -8,7 +9,8 @@ middlewares = {
             AuthorizationPlugin(),
             LanguagePlugin()
         ]
-    }
+    },
+    PrometheusMiddleware: {}
 }
 
 
--- app/api/route.py
+++ app/api/route.py
@@ -2,15 +2,18 @@ from app.api.v1.routers import sample_router as sample_router_v1
 from app.core.configs.app_config import app_config
 from app.core.routers import healthcheck_router
 from fastapi import FastAPI
+from starlette_prometheus import metrics
 
 _config = app_config()
 
 # Routers
 routers = [
     healthcheck_router.router,
-    sample_router_v1.router
+    sample_router_v1.router,
 ]
 
 
 def include_routers(app: FastAPI):
     [app.include_router(router, prefix=_config.prefix) for router in routers]
+
+    app.add_route('/metrics/', metrics)
