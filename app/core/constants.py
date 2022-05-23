75ea3a1961669a809c3470cf80919238bb79d853
Jonathan Leijendekker <jonathanleijendekker@gmail.com>
2022-05-23 12:40:53 +0800

    removed unused log constants

--- app/core/constants.py
+++ app/core/constants.py
@@ -4,9 +4,5 @@ from encodings.utf_8 import getregentry
 CONNECTION_PRIMARY = "primary"
 CONNECTION_READONLY = "readonly"
 
-# Logging
-LOG_TRACE_ID = "trace_id"
-LOG_SPAN_ID = "span_id"
-
 # Encoding
 UTF_8 = getregentry().name
